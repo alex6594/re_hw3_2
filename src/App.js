cfcf7f58add50013c9e19467cb486402970bec6a
AppVeyor <ci@appveyor.com>
2023-07-03 20:12:23 +0300

    fix homework

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import ListingPage from "components/ListingPage";
-import etsyData from "data/etsy.json";
-const App = () => {
-  //console.log(etsyData);
+import React from 'react';
+import './css/main.css'
+
+import Listing from './components/Listing';
+
+import data from './data/makeData';
+const actualData = [];
+for (const datum of data) {
+  const {
+    listing_id,
+    url,
+    MainImage,
+    title,
+    currency_code,
+    price,
+    quantity,
+  } = datum;
+  actualData.push({
+    listing_id,
+    url,
+    MainImage,
+    title,
+    currency_code,
+    price: (+price).toFixed(2),
+    quantity,
+  });
+}
+
+const finalData = actualData.filter(item => item.MainImage !== undefined);
+
+function App() {
   return (
-    <>
-      <ListingPage etsyData={etsyData} />
-    </>
+    <div className="App">
+      <Listing items={finalData} />
+    </div>
   );
-};
+}
 
 export default App;
