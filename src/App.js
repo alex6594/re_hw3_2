453cc165d68f96322529a7aa6de4798be808a79e
AppVeyor <ci@appveyor.com>
2023-07-04 21:34:39 +0300

    add jsx

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import React from 'react';
-import './css/main.css'
-
+import './App.css';
 import Listing from './components/Listing';
-
-import data from './data/makeData';
-const actualData = [];
-for (const datum of data) {
-  const {
-    listing_id,
-    url,
-    MainImage,
-    title,
-    currency_code,
-    price,
-    quantity,
-  } = datum;
-  actualData.push({
-    listing_id,
-    url,
-    MainImage,
-    title,
-    currency_code,
-    price: (+price).toFixed(2),
-    quantity,
-  });
-}
-
-const finalData = actualData.filter(item => item.MainImage !== undefined);
+import data from './data/etsy.json';
 
 function App() {
   return (
     <div className="App">
-      <Listing items={finalData} />
+      <Listing items={data} />
     </div>
   );
 }
